7b0945774fd9c3a5fec1f1534d85adf3e99132ee
Lazar Stanojevic <expbusinessit@gmail.com>
2021-10-12 13:42:21 +0200

    creating function that writes email and domain to .txt file

--- server-check.php
+++ server-check.php
@@ -1,8 +1,22 @@
 <?php
 
 /**
- * kada se klikne submit dugme cuvaj
- * email i domen u fajl
+ * creating function that will write email and domain
+ * of the user which will be later used to monitor website
+ * and send notification to the user's email
+ */
+function writeToFile($fileName)
+{
+    $emails = $_POST['email'];
+    $domains = $_POST['domain'];
+    if(!file_exists($fileName))
+        fopen($fileName, "w");
+    $emailsPlusDomains = $emails.' '.$domains;
+    fwrite($fileName,$emailsPlusDomains);
+}
+/**
+ * when user submits info calls the writeToFile function
+ * that writes email of an user and domain to txt file
  */
 if(isset($_POST['sbt']))
 {
