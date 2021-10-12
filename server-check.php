02432dfc09a551a7b1053293a8d91e03f852a8ae
Lazar Stanojevic <expbusinessit@gmail.com>
2021-10-12 15:41:13 +0200

    Creating function that checks if domain has https or not

--- server-check.php
+++ server-check.php
@@ -1,15 +1,33 @@
 <?php
 
+use JetBrains\PhpStorm\Internal\ReturnTypeContract;
+
 /**
  * creating function that will write email and domain
  * of the user which will be later used to monitor website
  * and send notification to the user's email
  */
+function isItHttps($domains)
+{
+    $httpsString = "https://";
+    if(strlen($domains) > 8)
+    {
+        for($i = 0; $i <8; $i++)
+        {
+            $domains[$i] == $httpsString[$i];
+        }
+    }
+    if($i == 8)
+        return $domains;
+    else return $httpsString.=$domains;
+}
+
 function writeToFile(string $fileName, string $emails, string $domains)
 {
     if (!file_exists($fileName))
         fopen($fileName, "w");
     $file = fopen($fileName, "a");
+    isItHttps($domains);
     $emailsPlusDomains = $emails . ' ' . $domains;
     fwrite($file, $emailsPlusDomains);
 }
