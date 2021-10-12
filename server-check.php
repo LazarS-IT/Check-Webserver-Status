8d08e2edb47b290edd4a612f552db90d82b6f0cd
Lazar Stanojevic <expbusinessit@gmail.com>
2021-10-12 12:56:57 +0200

    Setting file creation

--- server-check.php
+++ server-check.php
@@ -1,5 +1,20 @@
 <?php
 
+$emails = $_POST['email'];
+$domains = $_POST['domain'];
 
+/**
+ * kada se klikne submit dugme cuvaj
+ * email i domen u fajl
+ */
+if(isset($_POST['submit']))
+{
+    $fileWithDomens = fopen("emails-and-domens.txt","w") or die("unable to open file.");
+    $emailsPlusDomains = $emails.' '.$domains;
+    fwrite($fileWithDomens, $emails);
+}
+
+echo $emails;
+//echo $domains;
 
 ?>
