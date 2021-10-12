5cd8a212e3d5826cbedae8673499abc1ac1de82f
Lazar Stanojevic <expbusinessit@gmail.com>
2021-10-12 13:24:09 +0200

    setting script for creating and writing on file

--- server-check.php
+++ server-check.php
@@ -4,17 +4,21 @@
  * kada se klikne submit dugme cuvaj
  * email i domen u fajl
  */
-if(isset($_POST['submit']))
+if(isset($_POST['sbt']))
 {
     $emails = $_POST['email'];
     $domains = $_POST['domain'];
     $fileWithDomens = fopen("emails-and-domens.txt","w") or die("unable to open file.");
     $emailsPlusDomains = $emails.' '.$domains;
     fwrite($fileWithDomens, $emails);
-    
 }
+    ?>
 
-echo "helo";
-//echo $domains;
-
-?>
+<html>
+    <body>
+        <?php
+       $em = $_POST['email'];
+        echo $em;
+        ?>
+    </body>
+</html>
