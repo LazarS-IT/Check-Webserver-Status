9c4b298dd1bce7b687cf00ec1376a24f74b98957
Lazar Stanojevic <expbusinessit@gmail.com>
2021-10-28 13:37:17 +0200

    convert txt file to array

--- script-for-checking.php
+++ script-for-checking.php
@@ -7,8 +7,7 @@ ini_set('smtp_port', '25' );
 
 ini_set('allow_url_fopen', true); //enable fopen
 
-// define list of webservers to check
 $fileName = "domains.txt";
-$webservers = file($fileName, FILE_IGNORE_NEW_LINES);// array('www.example.com', 'www.example2.com');
+$webservers = file($fileName, FILE_IGNORE_NEW_LINES); // convert file to array of domains
 
 ?>
