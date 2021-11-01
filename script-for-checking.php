dcd28f0391619c7f18d0c408e7d9565900196c8f
Lazar Stanojevic <expbusinessit@gmail.com>
2021-11-01 14:35:32 +0100

    Setting email and smtp params

--- script-for-checking.php
+++ script-for-checking.php
@@ -6,7 +6,7 @@ use Consolidation\AnnotatedCommand\Cache\NullCache;
 
 ini_set('sendmail_from', 'support@gowpcare.com');
 ini_set('SMTP', '127.0.0.1');
-ini_set('smtp_port', '25');
+ini_set('smtp_port', '465');
 
 ini_set('allow_url_fopen', true); //enable fopen
 
