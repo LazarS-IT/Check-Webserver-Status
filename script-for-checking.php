dee7036558d73e23051582f25e09f0ef11130621
Lazar Stanojevic <expbusinessit@gmail.com>
2021-11-01 14:30:28 +0100

    Function for checking web server status

--- script-for-checking.php
+++ script-for-checking.php
@@ -32,3 +32,57 @@ function sendEmail($subject, string $message, $webservers)
     }
 }
 
+function getResponse($url, $webservers)
+{
+    $ch = curl_init(); // create cURL handle (ch)
+    if (!$ch) // sends an email if curl can't initialise
+    {
+        $subject = "Web Server Checking Script";
+        $message = "The server checking script issued and error when it tried to process" . $url .
+            ". Curl did not initialise correctly and issued and error - " . curl_error(curl_init()) .
+            "The script has died and not completed any more tasks.";
+        sendEmail($subject, $message, $webservers);
+        die();
+    }
+
+    //setting cURL options
+    $ret = curl_setopt($ch, CURLOPT_URL, "https://" . $url . "/");
+    $ret = curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+    $ret = curl_setopt($ch, CURLOPT_HEADER, true);
+    $ret = curl_setopt($ch, CURLOPT_NOBODY, true);
+    $ret = curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $ret = curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+    $ret = curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+    //execute
+    $ret = curl_exec($ch);
+
+    if (empty($ret)) {
+        // some kind of error happened
+        $subject = "Web Server Checking Script";
+        $message = "The server checking script issued an error when it tried to process" . $url .
+            ". Curl was trying to execute and issued the error - " . curl_error($ch) .
+            "Further URLs will be tried.";
+        sendEmail($subject, $message, $webservers);
+        curl_close($ch); //close cURL handler
+    } else {
+        $info = curl_getinfo($ch); //get header info - output is array
+        curl_close($ch);
+
+        if (empty($info['http_code'])) {
+            $subject = "Web Server Checking Script Error";
+            $message = "The server checking script issued an error when it tried to process " . $url . "\r\nNo HTTP code was returned";
+            sendEmail($subject, $message, $webservers);
+        } else {
+            //load the HTTP code descriptions
+            $http_codes = parse_ini_file("/server/path/to/http-response-code.ini"); //Change the path
+
+            // result - code number and description
+            $result = $info['http_code'] . " " . $http_codes[$info['http_code']];
+            return $result; // result contained a code, so return it
+        }
+        return Null; //info was empty so return nothing
+    }
+    return Null; // ret was empty so return nothing
+}
+
