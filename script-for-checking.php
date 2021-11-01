5c64988723a443912b9bf24e48fbfb43d6df260a
Lazar Stanojevic <expbusinessit@gmail.com>
2021-11-01 14:29:58 +0100

    Function for sending email

--- script-for-checking.php
+++ script-for-checking.php
@@ -10,3 +10,25 @@ ini_set('smtp_port', '25');
 
 ini_set('allow_url_fopen', true); //enable fopen
 
+$fileName = "emails-and-domains.txt";
+$webservers = file($fileName, FILE_IGNORE_NEW_LINES); // convert file to array of domains
+
+
+function sendEmail($subject, string $message, $webservers)
+{
+    $i = 0;
+    $j = 0;
+    $limit = sizeof($webservers);
+    while ($limit < 3) {
+        $wrapMessage = wordwrap($message, 70, "\n", true); // can't support more than 70 chars
+        $to = explode(" ", $webservers[$i]);
+        $headers = 'From: support@gowpcare.com' . "\r\n" .
+            'Reply-To: support@gowpcare.com' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+        return mail($to[$j], $subject, $wrapMessage, $headers); //send email
+        $i++;
+        $j += 2;
+    }
+}
+
