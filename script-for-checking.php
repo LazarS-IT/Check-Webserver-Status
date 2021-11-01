eb0ea81a6a126c819652082c71058fb37ffa7eab
Lazar Stanojevic <expbusinessit@gmail.com>
2021-11-01 14:29:16 +0100

    initialise email and server

--- script-for-checking.php
+++ script-for-checking.php
@@ -1,13 +1,12 @@
 <?php
 
 // set email server parameters
-ini_set('sendmail_from', 'support@gowpcare.com' );
-ini_set('SMTP', '127.0.0.1' );
-ini_set('smtp_port', '25' );
+
+use Consolidation\AnnotatedCommand\Cache\NullCache;
+
+ini_set('sendmail_from', 'support@gowpcare.com');
+ini_set('SMTP', '127.0.0.1');
+ini_set('smtp_port', '25');
 
 ini_set('allow_url_fopen', true); //enable fopen
 
-$fileName = "domains.txt";
-$webservers = file($fileName, FILE_IGNORE_NEW_LINES); // convert file to array of domains
-
-?>
